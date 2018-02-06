dd91e95b1510c9973d61160f6d79355eeb35337e
Rafal Gawel <rafal.gawel@miquido.com>
2018-02-06 11:09:41 +0100

    Create demo app Xcode project

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Rafał Gaweł on 06/02/2018.
+//  Copyright © 2018 Miquido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
